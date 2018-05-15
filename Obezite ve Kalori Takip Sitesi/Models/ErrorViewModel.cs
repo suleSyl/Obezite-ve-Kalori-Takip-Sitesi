a31ba8c3ac6370d838573221155377d597261cda
Şule <sule.syl11@gmail.com>
2018-05-15 10:27:42 +0300

    Add project file

---
+++ Obezite ve Kalori Takip Sitesi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Obezite_ve_Kalori_Takip_Sitesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
